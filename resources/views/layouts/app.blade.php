b954f18f21d91b75a17b24b1e5d591743a995fef
steven2307 <127774403+steven2307@users.noreply.github.com>
2025-02-10 16:02:32 +0000

    minor update (1)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,8 +46,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a href="{{ url('/') }}">
-                    <img src="storage/images/ibbi.png" class="ibbi">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
